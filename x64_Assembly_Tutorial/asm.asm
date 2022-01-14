06768e7064f6389907c8bf397be2001f609fe9f3
kavinda-ravishan <kvnravishan@outlook.com>
2022-01-14 16:45:51 +0530

    Calling Convention (Passing Parameters)

--- x64_Assembly_Tutorial/asm.asm
+++ x64_Assembly_Tutorial/asm.asm
@@ -1,6 +1,9 @@
 .data
 	var1 qword 0
 	var2 qword 0
+
+	floatVar real4 3.144
+	doubleVar real8 123.456
 .code
 
 ASMfunction1 proc
@@ -32,9 +35,31 @@ ASMfunction3 endp
 
 ASMfunction4 proc
 	
-
+	; xmm0 = input parameter
+	; return xmm0
 	ret
 
 ASMfunction4 endp
+
+ASMfunction5 proc
+	
+	mov rax, rcx
+	ret
+ASMfunction5 endp
+
+ASMfunction6 proc
+
+	movss xmm0, floatVar
+	ret
+
+ASMfunction6 endp
+
+ASMfunction7 proc
+
+	movsd xmm0, doubleVar
+	ret
+
+ASMfunction7 endp
+
 
 end
--- x64_Assembly_Tutorial/main.cpp
+++ x64_Assembly_Tutorial/main.cpp
@@ -11,6 +11,22 @@ extern "C" void ASMfunction3(
 
 extern "C" float ASMfunction4(float flt);
 
+class Entity
+{
+private:
+	unsigned long long x{ 1234 };
+public:
+	void print()
+	{
+		std::cout << x << std::endl;
+	}
+};
+
+extern "C" Entity* ASMfunction5(Entity & var);
+
+extern "C" float ASMfunction6();
+extern "C" double ASMfunction7();
+
 int main()
 {
 	std::cout << *ASMfunction1(123, 456) << std::endl;
@@ -22,6 +38,17 @@ int main()
 	std::cout << var2 << std::endl;
 
 	std::cout << ASMfunction4(123.123f) << std::endl;
+
+
+	Entity e;
+	Entity *eptr= ASMfunction5(e);
+	eptr->print();
+
+	float fptr = ASMfunction6();
+	std::cout << fptr << std::endl;
+
+	double dptr = ASMfunction7();
+	std::cout << dptr << std::endl;
 
 	return 0;
 }
