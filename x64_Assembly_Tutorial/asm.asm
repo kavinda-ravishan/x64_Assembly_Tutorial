b8d63c6b1a7eecad9b5a7b9e1a61f93f55f4b566
kavinda-ravishan <kvnravishan@outlook.com>
2022-01-15 18:59:56 +0530

    Control Structures (If statements, do while, while and for loops)

--- x64_Assembly_Tutorial/asm.asm
+++ x64_Assembly_Tutorial/asm.asm
@@ -1,24 +1,31 @@
 .data
-	var1 qword 0
-	var2 qword 0
 
-	floatVar real4 3.144
-	doubleVar real8 123.456
 .code
 
 ASMfunction1 proc
 	
-	mov var1, rcx
-	lea rax, qword ptr [var1]
+	xor rax, rax ; rax = 0
 
+	cmp rcx, rdx ; a, b
+	jl ECXLess ; jl : jump if less
+	jmp finished
+	
+ECXLess:
+	mov rax, 1
+
+
+finished:
 	ret
 
 ASMfunction1 endp
 
 ASMfunction2 proc
 	
-	mov var2, rdx
-	lea rax, qword ptr [var2]
+	xor rax, rax ; rax = 0
+	mov r8, 1
+
+	cmp rcx, rdx ; a, b
+	cmovl rax, r8 ; mov if less
 
 	ret
 
@@ -26,40 +33,83 @@ ASMfunction2 endp
 
 ASMfunction3 proc
 	
-	mov [r8], rcx
-	mov [r9], rdx
+	mov r8, 1
+
+	xor r9, r9
+	xor r10, r10
+	
+	cmp rcx, 10
+	cmove r9, r8 ; mov 1 to r9 ( if a == 10 )
+	cmp rdx, 20
+	cmove r10, r8 ; mov 1 to r10 (if b == 20)
+
+	and r9, r10 ; ( a == 10 and b == 20 )
+ 	mov rax, r9
 
 	ret
 
 ASMfunction3 endp
 
 ASMfunction4 proc
-	
-	; xmm0 = input parameter
-	; return xmm0
+
+	; rcx : arr
+	; rdx : size
+
+	xor r8, r8 ; counter
+
+	cmp r8, rdx
+	jge LoopEnd
+LoopHead:
+
+	mov qword ptr[rcx], r8
+	add rcx, 8
+
+	inc r8
+	cmp r8, rdx
+	jl LoopHead
+LoopEnd:
+
 	ret
 
 ASMfunction4 endp
 
 ASMfunction5 proc
-	
-	mov rax, rcx
+
+	; rcx : arr
+	; rdx : size
+
+	xor r8, r8 ; counter
+
+LoopHead:
+
+	mov qword ptr[rcx], r8
+	add rcx, 8
+
+	inc r8
+	cmp r8, rdx
+	jl LoopHead
+LoopEnd:
+
 	ret
+
 ASMfunction5 endp
 
 ASMfunction6 proc
 
-	movss xmm0, floatVar
+	; rcx : arr
+	; rdx : size
+    xor r8, r8
+
+LoopHead:
+
+	mov qword ptr[rcx+r8], rdx
+	add r8, 8
+
+	dec rdx
+	jnz LoopHead
+
 	ret
 
 ASMfunction6 endp
-
-ASMfunction7 proc
-
-	movsd xmm0, doubleVar
-	ret
-
-ASMfunction7 endp
-
 
 end
--- x64_Assembly_Tutorial/main.cpp
+++ x64_Assembly_Tutorial/main.cpp
@@ -1,54 +1,82 @@
 #include <iostream>
 
-extern "C" uint64_t * ASMfunction1(unsigned long long var1, unsigned long long var2);
-extern "C" uint64_t * ASMfunction2(unsigned long long var1, unsigned long long var2);
-extern "C" void ASMfunction3(
-	unsigned long long var1, 
-	unsigned long long var2,
-	unsigned long long* ptr1,
-	unsigned long long* ptr2
-);
+extern "C" uint64_t ASMfunction1(uint64_t a, uint64_t b);
+extern "C" uint64_t ASMfunction2(uint64_t a, uint64_t b);
+uint64_t Cppfunction1(uint64_t a, uint64_t b)
+{	
+	return 1 ? a<b : 0;
+}
 
-extern "C" float ASMfunction4(float flt);
-
-class Entity
+extern "C" uint64_t ASMfunction3(uint64_t a, uint64_t b);
+uint64_t Cppfunction3(uint64_t a, uint64_t b)
 {
-private:
-	unsigned long long x{ 1234 };
-public:
-	void print()
+	return 1 ? (a == 10 && b == 20) : 0;
+}
+
+extern "C" void ASMfunction4(uint64_t * arr, uint64_t size);
+void Cppfunction4(uint64_t* arr, uint64_t size)
+{
+	int i = 0;
+	while (i < size)
 	{
-		std::cout << x << std::endl;
+		*(arr + i) = i;
+		i++;
 	}
-};
+}
 
-extern "C" Entity* ASMfunction5(Entity & var);
+extern "C" void ASMfunction5(uint64_t * arr, uint64_t size);
+void Cppfunction5(uint64_t* arr, uint64_t size)
+{
+	int i = 0;
+	do 
+	{
+		*(arr + i) = i;
+		i++;
+	} while (i < size);
+}
 
-extern "C" float ASMfunction6();
-extern "C" double ASMfunction7();
+extern "C" void ASMfunction6(uint64_t * arr, uint64_t size);
+
+void printArr(uint64_t* arr, uint64_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout<<*(arr+i)<<" | ";
+	}
+	std::cout << std::endl;
+}
 
 int main()
 {
-	std::cout << *ASMfunction1(123, 456) << std::endl;
-	std::cout << *ASMfunction2(123, 456) << std::endl;
+	uint64_t a = 10;
+	uint64_t b = 20;
 
-	unsigned long long var1, var2;
-	ASMfunction3(123, 456, &var1, &var2);
-	std::cout << var1 << std::endl;
-	std::cout << var2 << std::endl;
+	std::cout << ASMfunction1(a, b) << std::endl;
+	std::cout << ASMfunction2(a, b) << std::endl;
+	std::cout << Cppfunction1(a, b) << std::endl;
 
-	std::cout << ASMfunction4(123.123f) << std::endl;
+	std::cout << ASMfunction3(a, b) << std::endl;
+	std::cout << Cppfunction3(a, b) << std::endl;
 
+	const uint64_t size = 20;
+	uint64_t arr1[size];
+	uint64_t arr2[size];
 
-	Entity e;
-	Entity *eptr= ASMfunction5(e);
-	eptr->print();
+	Cppfunction4(arr1, size);
+	printArr(arr1, size);
+	ASMfunction4(arr2, size);
+	printArr(arr2, size);
 
-	float fptr = ASMfunction6();
-	std::cout << fptr << std::endl;
+	uint64_t arr3[size];
+	uint64_t arr4[size];
+	Cppfunction5(arr3, size);
+	printArr(arr3, size);
+	ASMfunction5(arr4, size);
+	printArr(arr4, size);
 
-	double dptr = ASMfunction7();
-	std::cout << dptr << std::endl;
+	uint64_t arr5[size];
+	ASMfunction6(arr5, size);
+	printArr(arr5, size);
 
 	return 0;
 }
